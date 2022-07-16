d561e4ebfdda8c0593ba8ec4d028f6a0e8ce7d21
SANKET SHARMA <59464677+thesanketsharma@users.noreply.github.com>
2022-07-16 19:59:07 +0530

    Create git_setup.sh

---
+++ git_setup.sh
@@ -0,0 +1,6 @@
+git config --global user.name “thesanketsharma”
+git config --global user.email “ur.sanketsharma@gmail.com”
+git clone https://github.com/thesanketsharma/Data_Science_Projects.git
+cd Data_Science_Projects/
+git init
+git remote add origin https://github.com/thesanketsharma/Data_Science_Projects.git
